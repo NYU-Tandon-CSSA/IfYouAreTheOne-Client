2d284a5daddcb10c101216195a2aaa56845e0f62
biubiubiupiu21 <197yjy@gmail.com>
2023-03-26 22:11:25 -0400

    merge Light.js to Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,24 +1,38 @@
 import * as React from "react";
+
+import onLight from "../images/on.png";
+import offLight from "../images/off.png";
+import blastLight from "../images/blast.png";
+
 import image1 from "../images/userImage/image1.jpeg"
 import image2 from "../images/userImage/image2.jpeg"
 
-export default function image({name, mode }) {
-  let Image;
+export default function light({ userid, mode }) {
+  let light;
+  if (mode === "on") {
+    light = onLight;
+  } else if (mode === "off") {
+    light = offLight;
+
+  } else {
+    light = blastLight;
+
+  }
+
+  let userImage;
+  const images = [null, image1, image2, image1, image1, image2, image2, image2, image2, image2, image2, image2];
   let imageState;
-    if (name ==='1号'){
-        Image = image1
-    }else{
-        Image = image2
-    }
+  userImage = images[userid]
 
   if (mode === "on") {
     imageState = '100%'
   } else if (mode === "off") {
-
     imageState = '30%'
   } else {
-
     imageState = '100%'
   }
-  return (<img src={Image} width="95%" alt="light" style={{opacity:imageState, borderRadius: "50%"}}/>);
+  return (<div style={{position: "relative"}}>
+            <img src={userImage} width="100%" alt="light" style={{opacity:imageState, borderRadius: "50%", position: "relative"}}/>
+            <img src={light} width="60%" alt="light" style={{position: "relative",top:"-25%",left:"50%"}}/>
+          </div>);
 }
